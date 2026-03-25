4021ec247d09a18b5ac238f20a005ed21c9369f7
Albert Gomez <albert@agromaquina.net>
2026-03-25 22:58:23 +0100

    Limpiar archivos temporales y backups

--- start-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Next.js development server...
-echo Open http://localhost:3000 in your browser
-"C:\Program Files\nodejs\node.exe" node_modules\next\dist\bin\next.js dev
-pause
